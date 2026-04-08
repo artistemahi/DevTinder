02a5d21961fc331a50b03ea937e15e23390afe06
Mahesh kumar <vickymandal542@gmail.com>
2026-04-08 12:54:57 +0530

    learing http method basic

--- src/app.js
+++ src/app.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
-  console.log("sever started at port 3000...");
+  console.log("server started at port 3000...");
 });
-app.use("/about/2", (req, res) => {
-  res.send("hello from about 2 page");
+
+// get method , sending the data to the client
+app.get("/user", (req, res) => {
+  res.send({ id: 1, name: "john", age: 20 });
 });
-app.use("/about", (req, res) => {
-  res.send("this is  from about page");
+
+// patch method, updating the specific data of the user
+app.patch("/user/:id", (req, res) => {
+  console.log(`PATCH → updating user with id ${req.params.id}`, req.body);
+  res.send("user successfully updated with data by patch");
 });
-app.use("/profile", (req, res) => {
-  res.send("this is  from profile page");
+
+// post method, receiving the data from the client
+app.post("/user", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send({
+    message: "user successfully created",
+    user: data
+  });
 });
-app.use("/contact", (req, res) => {
-  res.send("this is  from contact page");
+
+// put method, updating the data of the user
+app.put("/user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const newData = req.body;
+
+  console.log(`PUT → updating user with id ${userId}`, newData);
+
+  res.send({
+    message: `user with id ${userId} successfully updated`,
+    user: newData
+  });
 });
-app.use("/", (req, res) => {
-  res.send("hello from homepage");
+// delete method, deleting the user
+app.delete("/user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  console.log(`DELETE → deleting user with id ${userId}`);
+  res.send({ message: `user with id ${userId} successfully deleted` });
 });
